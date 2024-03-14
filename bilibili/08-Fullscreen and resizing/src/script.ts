6c1d9867df81f1b3345ef8c6784bf60ade98a580
Honglei Wu <honglei.wu@shopee.com>
2024-03-14 09:51:52 +0800

    feat: 上传 08 初始代码

---
+++ bilibili/08-Fullscreen and resizing/src/script.ts
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+console.info("OrbitControls", OrbitControls);
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas!,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+
+const tick = () => {
+  requestAnimationFrame(tick);
+
+
+  renderer.render(scene, camera);
+};
+
+tick();
