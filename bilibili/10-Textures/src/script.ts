c161d71f28ef4e504f61bc695ba78a8c83266649
wuhonglei <1017368065@qq.com>
2024-05-21 23:43:01 +0800

    feat: 学习 material matCapTexture

--- bilibili/10-Textures/src/script.ts
+++ bilibili/10-Textures/src/script.ts
@@ -6,6 +6,8 @@ import iceUrl from "./assets/ice_surface.jpeg";
 import checkerboardUrl from "./assets/checkerboard-1024x1024.png";
 import checkerboardUrlSmall from "./assets/checkerboard-8×8.png";
 import metalUrl from "./assets/Metal_pattern_007d.png";
+import alphaUrl from "./assets/alphamap.png";
+import matCapUrl from "./assets/matcaps/6.png";
 
 /**
  * Base
@@ -53,11 +55,11 @@ const scene = new THREE.Scene();
 const manager = new THREE.LoadingManager();
 
 manager.onLoad = () => {
-  console.log("onLoad");
+  // console.log("onLoad");
 };
 
 manager.onProgress = (url, itemsLoaded, itemsTotal) => {
-  console.log("onProgress", url, itemsLoaded, itemsTotal);
+  // console.log("onProgress", url, itemsLoaded, itemsTotal);
 };
 
 manager.onError = (url) => {
@@ -70,23 +72,51 @@ const iceTexture = textureLoader.load(iceUrl);
 const checkerboardTexture = textureLoader.load(checkerboardUrl);
 const checkerboardTextureSmall = textureLoader.load(checkerboardUrlSmall);
 const metalTexture = textureLoader.load(metalUrl);
-
-iceTexture.minFilter = THREE.NearestFilter;
-iceTexture.magFilter = THREE.NearestFilter;
-iceTexture.generateMipmaps = false;
+const alphaTexture = textureLoader.load(alphaUrl);
+const matCapTexture = textureLoader.load(matCapUrl);
 
 // 立方体
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+// 球体
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+
+// plane 平面
+const planeGeometry = new THREE.PlaneGeometry(1, 1, 100, 100);
+
+// torus
+const torusGeometry = new THREE.TorusGeometry(0.4, 0.2, 16, 32);
+
+// const material = new THREE.MeshBasicMaterial({
+//   // color: "purple",
+//   map: iceTexture,
+//   // alphaMap: alphaTexture,
+//   transparent: true, // 启用透明度
+//   opacity: 1, // 设置不透明度
+// });
+// material.side = THREE.DoubleSide;
+
+// const material = new THREE.MeshNormalMaterial({
+//   wireframe: false,
+//   wireframeLinewidth: 5,
+//   flatShading: true,
+// });
+
+const material = new THREE.MeshMatcapMaterial({
+  matcap: matCapTexture,
+});
 
 // Object
-const mesh = new THREE.Mesh(
-  geometry,
-  new THREE.MeshBasicMaterial({
-    map: iceTexture,
-    wireframe: false,
-  })
-);
-scene.add(mesh);
+const boxMesh = new THREE.Mesh(boxGeometry, material);
+const sphereMesh = new THREE.Mesh(sphereGeometry, material);
+const planeMesh = new THREE.Mesh(planeGeometry, material);
+const torusMesh = new THREE.Mesh(torusGeometry, material);
+
+boxMesh.position.x = -1.5;
+sphereMesh.position.x = 0;
+torusMesh.position.x = 1.5;
+
+scene.add(boxMesh, sphereMesh, planeMesh, torusMesh);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -96,7 +126,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.set(0, 0, 3);
-camera.lookAt(mesh.position);
+camera.lookAt(boxMesh.position);
 scene.add(camera);
 
 // Renderer
