1152b22c6d3d456c2bb4f682328250f4d557ea9b
wuhonglei <1017368065@qq.com>
2024-06-05 00:17:02 +0800

    feat: 加载字体，显示文字

--- bilibili/12-3D Text/src/script.ts
+++ bilibili/12-3D Text/src/script.ts
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+
 /**
  * Base
  */
@@ -44,12 +47,45 @@ window.addEventListener("dblclick", () => {
 // Scene
 const scene = new THREE.Scene();
 
+const loader = new FontLoader();
+loader.load("fonts/optimer_regular.typeface.json", (font) => {
+  console.info("font", font);
+  // 创建文本几何体
+  const textGeometry = new TextGeometry("Hello Three.js", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 20,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.03,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+
+  // 创建材质
+  const material = new THREE.MeshBasicMaterial({
+    color: "red",
+    wireframe: true,
+  });
+
+  // 创建网格并添加到场景
+  const textMesh = new THREE.Mesh(textGeometry, material);
+  // 将字体放在中心
+  // textGeometry.computeBoundingBox();
+  textGeometry.center();
+  // 打印字体位置
+  console.info("textGeometry.boundingBox", textGeometry.boundingBox);
+
+  scene.add(textMesh);
+});
+
 // Object
 const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false })
 );
-scene.add(mesh);
+// scene.add(mesh);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
